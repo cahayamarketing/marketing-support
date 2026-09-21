5cfb364a291434786ba9d5c4b30454f9561db2d0
cahayamarketing <cahayamarketing@gmail.com>
2026-09-21 21:44:07 +0700

    login perbaikan

--- app.js
+++ app.js
@@ -2610,7 +2610,10 @@
 
         leasingFund.addEventListener(
             "input",
-            calculateTotalFund
+            function () {
+                calculateTotalFund();
+                updateBudgetDetailSummary();
+            }
         );
 
         row
